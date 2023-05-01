1fbdc7ab35c8fb02af6d42156536594426450d32
nigarumovum <bfpr131095@gmail.com>
2023-05-01 15:21:34 -0600

    linux

--- Linux/2.PrintElapsedTimeOfCodeExecution.sh
+++ Linux/2.PrintElapsedTimeOfCodeExecution.sh
@@ -3,6 +3,7 @@
 start_time=$(date +%s)
 
 # your code here
+# python script.py
 
 end_time=$(date +%s)
 
--- Linux/5.ExecuteOnEachFileIf.sh
+++ Linux/5.ExecuteOnEachFileIf.sh
@@ -1,4 +1,3 @@
-
 for file in *; do
     if [[ $file =~ ^[a-zA-Z] ]]; then
         # execute command on the file
